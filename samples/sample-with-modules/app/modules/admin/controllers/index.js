6e42529f3d9759a95a4c26e9739f2b56439e09f7
morozovsk <morozovsk@gmail.com>
2012-06-14 04:05:40 +0400

    0.0.1

--- samples/sample-with-modules/app/modules/admin/controllers/index.js
+++ samples/sample-with-modules/app/modules/admin/controllers/index.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     this.indexAction = function() {
         var model = this._model('message');
-        this._view.info = model.getInfo();
+        this._local('info', model.getInfo());
     }
 }
